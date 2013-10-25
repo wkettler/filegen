577b1fcdc7d9454d9d5395f0bbbd06ee39686864
William Kettler <william.p.kettler@gmail.com>
2013-10-25 08:05:22 -0700

    - Added GPLv3 license. - Added filegen function that is called by main for portability.

--- filegen.py
+++ filegen.py
@@ -1,35 +1,47 @@
-#! /usr/bin/env python
-#
-# filegen.py
-#
-# File generation utility.
-#
+#!/usr/bin/python
 
-__author__  = 'William Kettler <william_kettler@dell.com>'
-__created__ = 'January 31, 2013'
+"""
+filegen.py
+
+File generation utility.
+
+Copyright (C) 2013  William Kettler <william.p.kettler@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import os
 from random import randint
 from argparse import ArgumentParser
 
-def trim_slash(d):
-    """
-    Trim trailing slash.
-    """
-    return d.rstrip(os.path.sep)
-
-def srand(f, size, bs=None):
+def w_srand(f, size, bs=None):
     """
     Create a new file and fill it with pseudo random data.
-    
-    Arguments:
-    f -- name of the file
-    size -- size in KB
-    bs -- block size in KB
+        
+    Inputs:
+        f    (str): name of file
+        size (int): size in KB
+        bs   (int): block size in KB
+    Outputs:
+        NULL
     """
-   
+    
     if not bs:
-        bs = size
+        if size > 1024:
+            bs = 1024
+        else:
+            bs = size
    
     buf = os.urandom(1024)
     with open(f, 'w') as fh:
@@ -41,31 +53,30 @@ def srand(f, size, bs=None):
                 fh.write(size * buf)
                 break
         fh.flush()
-
-if __name__ == '__main__':
-    # Define CLI arguments.
-    parser = ArgumentParser(description='File generation utility.')
-    parser.add_argument('--dst', dest='dst', type=str, required=True,
-        help='destination directory')
-    parser.add_argument('--min', dest='min', type=int, required=True,
-        help='minimum file size in KB')
-    parser.add_argument('--max', dest='max', type=int, required=True,
-        help='max file size in KB')
-    parser.add_argument('--qty', dest='qty', type=int, required=True,
-        help='file count')
-    parser.add_argument('--split', dest='split', type=int, required=True,
-        help='files per directory')
-    args = parser.parse_args()
+        
+def filegen(min_sz, max_sz, qty, dst=None, split=None):
+    """
+        Generate files.
+        
+        Inputs:
+            min_sz (int): Minimum file size
+            max_sz (int): Maximum file size
+            qty    (int): Total file count
+            dst    (str): Destination directory
+            split  (int): File per directory
+        Outputs:
+            NULL
+    """
     
-    # Load values
-    dst = args.dst
-    min_sz = args.min
-    max_sz = args.max
-    split = args.split
-    qty = args.qty
-    
-    current_dir = 0
-    os.mkdir(os.path.join(dst, str(current_dir)))
+    if not dst:
+        dst = os.getcwd()
+        
+    if split:
+        current_dir = 0
+        os.mkdir(os.path.join(dst, str(current_dir)))
+    else:
+        current_dir = dst
+        split = qty
     
     current_ct = 0
     while True:
@@ -82,8 +93,25 @@ if __name__ == '__main__':
         # Write file.    
         size = randint(min_sz, max_sz)
         f = os.path.join(dst, str(current_dir), ".".join([str(current_ct), "data"]))
-        srand(f, size)
+        w_srand(f, size)
         
         # Update counters.
         current_ct += 1
         qty -= 1
+
+if __name__ == '__main__':
+    # Define CLI arguments.
+    parser = ArgumentParser(description='File generation utility.')
+    parser.add_argument('--min', dest='min', type=int, required=True,
+        help='minimum file size in KB')
+    parser.add_argument('--max', dest='max', type=int, required=True,
+        help='max file size in KB')
+    parser.add_argument('--qty', dest='qty', type=int, required=True,
+        help='file count')
+    parser.add_argument('--dst', dest='dst', type=str, required=False,
+        default=None, help='destination directory')
+    parser.add_argument('--split', dest='split', type=int, required=False,
+        default=None, help='files per directory')
+    args = parser.parse_args()
+    
+    filegen(args.min, args.max, args.qty, args.dst, args.split)
